52bcf8996a5eaf43aa1ed6b3efe2ece1f9133615
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-02-24 20:37:58 +0900

    feat: link entries with a submodule

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,14 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  env: {
+    schema: {
+      DEV_ONLY_CONTENTS_PATH: envField.string({
+        access: "secret",
+        context: "server",
+        optional: true,
+      }),
+    },
+  },
+});
