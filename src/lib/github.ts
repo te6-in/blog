ac0957e2ba2c74f20474e570e415f985e982224f
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-08-17 23:35:37 +0900

    refactor: refactor & some fixes

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -65,3 +65,50 @@ export type DiscussionsResponse = {
   };
   timeline_url: string;
 }[];
+
+export type DiscussionCommentsResponse = {
+  id: number;
+  node_id: string;
+  html_url: string;
+  parent_id: number | null;
+  child_comment_count: number;
+  repository_url: string;
+  discussion_id: number;
+  author_association: string;
+  user: {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    user_view_type: string;
+    site_admin: boolean;
+  };
+  created_at: string;
+  updated_at: string;
+  body: string;
+  reactions: {
+    url: string;
+    total_count: number;
+    "+1": number;
+    "-1": number;
+    laugh: number;
+    hooray: number;
+    confused: number;
+    heart: number;
+    rocket: number;
+    eyes: number;
+  };
+}[];
